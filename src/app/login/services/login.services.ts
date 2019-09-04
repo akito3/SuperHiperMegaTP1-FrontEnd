f3d6bf5648fc6b337dc8dab8d7dcdf180b14bbeb
akito3 <54566735+akito3@users.noreply.github.com>
2019-09-03 23:27:58 -0400

    Update login.services.ts

--- src/app/login/services/login.services.ts
+++ src/app/login/services/login.services.ts
@@ -1,48 +1,55 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { API_ENDPOINT } from '../../util/constants';
-import { Observable, EMPTY, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { element } from 'protractor';
-import { EmptyObservable } from 'rxjs/observable/EmptyObservable';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/login.services';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
-
-@Injectable({
-    providedIn: 'root'
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LoginService {
-    constructor(private httpClient: HttpClient) {
-    }
+export class LoginComponent implements OnInit {
+  public username: string = "";
+  public password: string = "";
+  public showSpinner : boolean;
+  public errorMessage : string =""
+  public hasError : boolean;
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+
+  
+  }
 
 
+  ngOnInit() {
 
-    loginUsuario(usuario: String, password: String): Observable<any> {
+  }
 
-        const headers = new HttpHeaders({
+  login() {
+    this.showSpinner = true;// spinner on
+    this.loginService.loginUsuario(this.username, this.password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+          //faltaria pasar el user
+          this.hasError = false
+          this.router.navigate(['../../dashboard'], { relativeTo: this.route });
+        },
+        error => {
+          console.log('Error ! ', error.message);
+          this.hasError = true
+          this.errorMessage = error.message
+        }).add(() => {
 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-
+          this.showSpinner = false;
+    
         });
-        const url: string = API_ENDPOINT + 'stock-pwfe/persona'
-        return this.httpClient.get(url, { headers: headers }).
-            pipe(
-                map(
-                    (objeto) =>
-                        objeto['lista'].filter(elemento => elemento['usuarioLogin'] != null && elemento['usuarioLogin'] == usuario && elemento['cedula'] == password)
-                ),
-                map((objeto: any) => {
-                    if (objeto.length == 0) {
-                        throw new Error("Usuario/Contrasena Invalido")
-                    } else {
-                        return objeto;
-                    }
 
-                }
-                )
-            )
-
-    }
-
+  }
 
 }
